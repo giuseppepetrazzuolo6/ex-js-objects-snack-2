d26e8b2505ca93e5cf490456c8dd8718803971ed
giuseppepetrazzuolo6 <giuseppepetrazzuolo6@gmail.com>
2026-02-06 11:04:00 +0100

    snack-2

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 //🏆 Code Question 1
-const hamburger = { name: "Cheese Burger", weight: 250 };
+//const hamburger = { name: "Cheese Burger", weight: 250 };
 const secondBurger = hamburger;
 secondBurger.name = 'Double Cheese Burger';
 secondBurger.weight = 500;
@@ -9,3 +9,21 @@ console.log(secondBurger.name); // ?
 //Senza lanciare il codice, riesci a prevedere cosa viene stampato in console? //Double Cheese Burger
 
 //Quanti oggetti sono stati creati in memoria durante l'esecuzione di questo codice? //1
+
+
+//🏆 Code Question 2
+const hamburger = {
+    name: "Cheese Burger",
+    weight: 250,
+    ingredients: ["Cheese", "Meat", "Bread", "Tomato"]
+};
+
+const secondBurger = { ...hamburger };
+secondBurger.ingredients[0] = "Salad";
+
+console.log(hamburger.ingredients[0]); // ?
+console.log(secondBurger.ingredients[0]); // ?
+//P.S.: Ricordati che gli Array, come gli oggetti, sono dei Reference Type (Tipi di Riferimento)!
+//Senza lanciare il codice, riesci a prevedere cosa viene stampato in console? //salad
+//Quanti oggetti sono stati creati in memoria durante l'esecuzione di questo codice?
+//2 oggetti, ma la proprietà ingredients viene copiata per riferimento, quindi verrà modificata in entrambi gli oggetti.
